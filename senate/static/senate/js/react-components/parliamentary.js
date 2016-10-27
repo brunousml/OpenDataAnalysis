3a317ba172d9274089563afeb7e9b87e1b76a371
Bruno Melo <brunousml@gmail.com>
2016-10-27 20:28:45 -0200

    list parliaments

--- senate/static/senate/js/react-components/parliamentary.js
+++ senate/static/senate/js/react-components/parliamentary.js
@@ -2,21 +2,21 @@ var Parliamentary = React.createClass({
     render: function(){
         console.log(this.props.p);
         return(
-            <div className="col-md-3 col-sm-6 hero-feature" >
-                <div className="thumbnail" style={{'width': '250px', 'height': '600px'}}>
-                    <img src={this.props.p.identification.url_photo} alt="" />
+            <div className="col-md-3 col-sm-20 hero-feature" style={{'height':'700px'}} >
+                <div className="thumbnail" >
+                    <img src={this.props.p.identification.url_photo} style={{'width':'100%'}} alt="" />
                     <div className="caption">
-                        <h3>{this.props.p.identification.name}</h3>
+                        <h3>{this.props.p.identification.salutation} {this.props.p.identification.name}</h3>
                         <div className="info-p">
+                            <h4>Atuação no Parlamentar:</h4>
                             <ul>
-                                <li><strong>Cargo</strong>: {this.props.p.identification.salutation}</li>
-                                <li><strong>Materias: </strong>: {this.props.p.matters.length}</li>
-                                <li><strong>Comissoes: </strong>: {this.props.p.commissions.length}</li>
+                                <li><strong>Matérias: </strong>: {this.props.p.matters.length}</li>
+                                <li><strong>Comissões: </strong>: {this.props.p.commissions.length}</li>
                                 <li><strong>Relatórios: </strong>: {this.props.p.reports.length}</li>
                             </ul>
                             <h4>Contatos:</h4>
-                            <p><strong>Telefone: </strong>: {this.props.p.phone}</p>
-                            <p><strong>Email: </strong>: {this.props.p.identification.email}</p>
+                            <p><strong>Telefone: </strong>: <br/>{this.props.p.phone}</p>
+                            <p><strong>Email: </strong>: <br/>{this.props.p.identification.email}</p>
                         </div>
                     </div>
                 </div>
@@ -76,6 +76,6 @@ var Parliamentarians = React.createClass({
 });
 
 ReactDOM.render(
-  <Parliamentarians  url='http://localhost:8000/api/v1/parliamentary?format=jsonp&limit=100'/>,
+  <Parliamentarians  url='http://localhost:8000/api/v1/parliamentary?format=jsonp&limit=200'/>,
   document.getElementById('to-render')
 );
